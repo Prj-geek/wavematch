b2b7e09f4585f9868498d58445d16a0daf48a9b3
Prj-geek <2727priyanshu@gmail.com>
2026-01-17 15:14:35 +0530

    Use it in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,16 @@
+import Login from "./components/Login";
+import Protected from "./components/Protected";
+import { AuthProvider } from "./context/AuthContext";
 
+function App() {
+  return (
+    <AuthProvider>
+      <Login />
+      <Protected>
+        <p>You are logged in</p>
+      </Protected>
+    </AuthProvider>
+  );
+}
+
+export default App;

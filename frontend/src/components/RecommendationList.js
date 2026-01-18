68e9587ea701a23d6b79fbf93776400559e4d2c7
Prj-geek <2727priyanshu@gmail.com>
2026-01-18 20:34:14 +0530

    List of results

--- frontend/src/components/RecommendationList.js
+++ frontend/src/components/RecommendationList.js
@@ -1,1 +1,16 @@
+import SongCard from "./SongCard";
 
+export default function RecommendationList({ recommendations }) {
+  if (!recommendations || !recommendations.length) {
+    return <p>No recommendations yet</p>;
+  }
+
+  return (
+    <div>
+      <h3>Recommendations</h3>
+      {recommendations.map((song, index) => (
+        <SongCard key={index} song={song} />
+      ))}
+    </div>
+  );
+}

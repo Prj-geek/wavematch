3baaee165c100435158b6f63e323704d243ddfa0
Prj-geek <2727priyanshu@gmail.com>
2026-01-18 20:31:20 +0530

    Page level compostiotn

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,19 @@
+import Login from "../components/Login";
+import Protected from "../components/Protected";
+import RecommendButton from "../components/RecommendButton";
+import Playlists from "../components/Playlists";
 
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>WaveMatch</h1>
+
+      <Login />
+
+      <Protected>
+        <RecommendButton />
+        <Playlists />
+      </Protected>
+    </div>
+  );
+}

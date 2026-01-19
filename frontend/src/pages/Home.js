64dc9798ef4d91a986cba78421ed7387e34f6099
Prj-geek <2727priyanshu@gmail.com>
2026-01-19 18:58:33 +0530

    updated home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Login from "../components/Login";
 import Protected from "../components/Protected";
 import SearchBar from "../components/SearchBar";
+import MultiSearchBar from "../components/MultiSearchBar";
 import RecommendationList from "../components/RecommendationList";
 import Playlists from "../components/Playlists";
 
@@ -15,7 +16,12 @@ export default function Home() {
       <Login />
 
       <Protected>
+        <h3>Single Song</h3>
         <SearchBar onResults={setRecommendations} />
+
+        <h3>Playlist Seeds</h3>
+        <MultiSearchBar onResults={setRecommendations} />
+
         <RecommendationList recommendations={recommendations} />
         <Playlists />
       </Protected>
